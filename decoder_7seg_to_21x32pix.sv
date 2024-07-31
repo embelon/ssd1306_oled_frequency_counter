034544de5968f4a3d26f3daccf80714b6d1e4f01
embelon <78412338+embelon@users.noreply.github.com>
2024-07-31 17:50:43 +0200

    Fixed syntax issues in 7seg to 21x32 decoder

--- decoder_7seg_to_21x32pix.sv
+++ decoder_7seg_to_21x32pix.sv
@@ -7,17 +7,17 @@ module decoder_7seg_to_21x32pix
     input [3:0] index_x,
     input [1:0] index_y,
 
-    output [7:0] pix_column
+    output [7:0] pixels_column
 );
 
     parameter SPACE = 2;
 
     // adjust space before character (number of empty columns)
-    wire [3:0] x = index_x - SPACE;
-    wire [1:0] y = index_y;    
+    logic [3:0] x = index_x - SPACE;
+    logic [1:0] y = index_y;    
 
     // 'b', 'c', 'e', 'f' segments definition
-    wire [7:0] pattern_bcef[8] = 
+    logic [7:0] pattern_bcef[8] = 
     {
         8'h0f, 
         8'hfc, 
@@ -32,7 +32,7 @@ module decoder_7seg_to_21x32pix
     // 'a' and 'd' segments are packed with 'g' segments in one table to save space
     parameter ad_mask = 8'hf8;
     parameter g_mask = 8'h03;
-    wire [7:0] pattern_adg[12] = {
+    logic [7:0] pattern_adg[12] = {
         8'h40, 
         8'h60 | 8'h01,
         8'h70 | 8'h03,
@@ -47,66 +47,71 @@ module decoder_7seg_to_21x32pix
         8'h40
     };
 
-    function [7:0] segment_A(input Segments segments, wire [3:0] x, wire [1:0] y);
+    function [7:0] segment_A(input Segments segments, logic [3:0] x, logic [1:0] y);
+        logic [7:0] pixels = 8'h00;
         if (segments.individual.a && (x >= 2) && (x <= 13) && (y == 0)) begin
-            segment_A = ad_mask & pattern_adg[x - 2];
-        end else begin
-            segment_A = 8'h00;
+            pixels = ad_mask & pattern_adg[x - 2];
         end
+        segment_A = pixels;
     endfunction
 
-    function [7:0] segment_D(input Segments segments, wire [3:0] x, wire [1:0] y);
+    function [7:0] segment_D(input Segments segments, logic [3:0] x, logic [1:0] y);
+        logic [7:0] pixels = 8'h00;
         if (segments.individual.d &&(x >= 2) && (index_x <= 13) && (y == 3)) begin
-            segment_D = ad_mask & reverse_8bits(pattern_adg[x - 2]);
-        end else begin
-            segment_D = 8'h00;
+            pixels = ad_mask & reverse_8bits(pattern_adg[x - 2]);
         end
+        segment_D = pixels;
     endfunction
 
-    function [7:0] segment_G(input Segments segments, wire [3:0] x, wire [1:0] y);
+    function [7:0] segment_G(input Segments segments, logic [3:0] x, logic [1:0] y);
+        logic [7:0] pixels = 8'h00;
         if (segments.individual.g && (x >= 2) && (x <= 13)) begin
             if (y == 1) begin
-                return g_mask & pattern_adg[x - 2];
+                pixels = g_mask & pattern_adg[x - 2];
             end else if (y == 2) begin
-                return g_mask & reverse_8bits(pattern_adg[x - 2]);
+                pixels =  g_mask & reverse_8bits(pattern_adg[x - 2]);
             end 
         end
-        return 0'h00;        
+        segment_G = pixels;      
     endfunction
 
-    function [7:0] segment_B(input Segments segments, [3:0] x, [1:0] y)
+    function [7:0] segment_B(input Segments segments, logic [3:0] x, logic [1:0] y);
+        logic [7:0] pixels = 8'h00;
         if (segments.individual.b && (x >= 12) && (x <= 15) && ((y == 0) || (y == 1))) begin
-            return pattern_bcef[{!x[1:0], y[0]}];
+            pixels = pattern_bcef[{!x[1:0], y[0]}];
         end
-        return 0'h00;
+        segment_B = pixels;
     endfunction
 
-    function [7:0] segment_C(input Segments segments, [3:0] x, [1:0] y)
+    function [7:0] segment_C(input Segments segments, logic [3:0] x, logic [1:0] y);
+        logic [7:0] pixels = 8'h00;
         if (segments.individual.c && (x >= 12) && (x <= 15) && ((y == 2) || (y == 3))) begin
-            return reverse_8bits(pattern_bcef[{!x[1:0], !y[0]}]);
+            pixels = reverse_8bits(pattern_bcef[{!x[1:0], !y[0]}]);
         end
-        return 0'h00;
+        segment_C = pixels;
     endfunction
 
-    function [7:0] segment_E(input Segments segments, [3:0] x, [1:0] y)
+    function [7:0] segment_E(input Segments segments, logic [3:0] x, logic [1:0] y);
+        logic [7:0] pixels = 8'h00;
         if (segments.individual.e && (x <= 3) && ((y == 2) || (y == 3))) begin
-            return reverse_8bits(pattern_bcef[{x[1:0], !y[0]}]);
+            pixels = reverse_8bits(pattern_bcef[{x[1:0], !y[0]}]);
         end
-        return 0'h00;
+        segment_E = pixels;
     endfunction
 
-    function [7:0] segment_F(input Segments segments, [3:0] x, [1:0] y)
+    function [7:0] segment_F(input Segments segments, logic [3:0] x, logic [1:0] y);
+        logic [7:0] pixels = 8'h00;
         if (segments.individual.f && (x <= 3) && ((y == 0) || (y == 1))) begin
-            return pattern_bcef[{x[1:0], y[0]}];
+            pixels = pattern_bcef[{x[1:0], y[0]}];
         end
-        return 0'h00;
+        segment_F = pixels;
     endfunction
 
 always @(digit_in, index_x, index_y) begin
     if (index_x < SPACE) begin
-        pix_column <= 8'h00;
+        pixels_column <= 8'h00;
     end else begin
-        pix_column <= segment_A(segments_in, x, y) ||
+        pixels_column <= segment_A(segments_in, x, y) ||
             segment_B(segments_in, x, y) ||
             segment_C(segments_in, x, y) ||
             segment_D(segments_in, x, y) ||
