5bd8f329994b6fe4f0627cb8dafa57d1ece9217e
embelon <78412338+embelon@users.noreply.github.com>
2024-07-31 18:43:07 +0200

    Minor adjustments in 7seg to 21x32 decoder

--- decoder_7seg_to_21x32pix.sv
+++ decoder_7seg_to_21x32pix.sv
@@ -13,11 +13,11 @@ module decoder_7seg_to_21x32pix
     parameter SPACE = 2;
 
     // adjust space before character (number of empty columns)
-    logic [3:0] x = index_x - SPACE;
-    logic [1:0] y = index_y;    
+    bit [3:0] x = index_x - SPACE;
+    bit [1:0] y = index_y;    
 
     // 'b', 'c', 'e', 'f' segments definition
-    logic [7:0] pattern_bcef[8] = 
+    bit [7:0] pattern_bcef[8] = 
     {
         8'h0f, 
         8'hfc, 
@@ -32,7 +32,7 @@ module decoder_7seg_to_21x32pix
     // 'a' and 'd' segments are packed with 'g' segments in one table to save space
     parameter ad_mask = 8'hf8;
     parameter g_mask = 8'h03;
-    logic [7:0] pattern_adg[12] = {
+    bit [7:0] pattern_adg[12] = {
         8'h40, 
         8'h60 | 8'h01,
         8'h70 | 8'h03,
@@ -47,24 +47,24 @@ module decoder_7seg_to_21x32pix
         8'h40
     };
 
-    function [7:0] segment_A(input Segments segments, logic [3:0] x, logic [1:0] y);
-        logic [7:0] pixels = 8'h00;
+    function [7:0] segment_A(input Segments segments, bit [3:0] x, bit [1:0] y);
+        bit [7:0] pixels = 8'h00;
         if (segments.individual.a && (x >= 2) && (x <= 13) && (y == 0)) begin
             pixels = ad_mask & pattern_adg[x - 2];
         end
         segment_A = pixels;
     endfunction
 
-    function [7:0] segment_D(input Segments segments, logic [3:0] x, logic [1:0] y);
-        logic [7:0] pixels = 8'h00;
+    function [7:0] segment_D(input Segments segments, bit [3:0] x, bit [1:0] y);
+        bit [7:0] pixels = 8'h00;
         if (segments.individual.d &&(x >= 2) && (index_x <= 13) && (y == 3)) begin
             pixels = ad_mask & reverse_8bits(pattern_adg[x - 2]);
         end
         segment_D = pixels;
     endfunction
 
-    function [7:0] segment_G(input Segments segments, logic [3:0] x, logic [1:0] y);
-        logic [7:0] pixels = 8'h00;
+    function [7:0] segment_G(input Segments segments, bit [3:0] x, bit [1:0] y);
+        bit [7:0] pixels = 8'h00;
         if (segments.individual.g && (x >= 2) && (x <= 13)) begin
             if (y == 1) begin
                 pixels = g_mask & pattern_adg[x - 2];
@@ -75,32 +75,32 @@ module decoder_7seg_to_21x32pix
         segment_G = pixels;      
     endfunction
 
-    function [7:0] segment_B(input Segments segments, logic [3:0] x, logic [1:0] y);
-        logic [7:0] pixels = 8'h00;
+    function [7:0] segment_B(input Segments segments, bit [3:0] x, bit [1:0] y);
+        bit [7:0] pixels = 8'h00;
         if (segments.individual.b && (x >= 12) && (x <= 15) && ((y == 0) || (y == 1))) begin
             pixels = pattern_bcef[{!x[1:0], y[0]}];
         end
         segment_B = pixels;
     endfunction
 
-    function [7:0] segment_C(input Segments segments, logic [3:0] x, logic [1:0] y);
-        logic [7:0] pixels = 8'h00;
+    function [7:0] segment_C(input Segments segments, bit [3:0] x, bit [1:0] y);
+        bit [7:0] pixels = 8'h00;
         if (segments.individual.c && (x >= 12) && (x <= 15) && ((y == 2) || (y == 3))) begin
             pixels = reverse_8bits(pattern_bcef[{!x[1:0], !y[0]}]);
         end
         segment_C = pixels;
     endfunction
 
-    function [7:0] segment_E(input Segments segments, logic [3:0] x, logic [1:0] y);
-        logic [7:0] pixels = 8'h00;
+    function [7:0] segment_E(input Segments segments, bit [3:0] x, bit [1:0] y);
+        bit [7:0] pixels = 8'h00;
         if (segments.individual.e && (x <= 3) && ((y == 2) || (y == 3))) begin
             pixels = reverse_8bits(pattern_bcef[{x[1:0], !y[0]}]);
         end
         segment_E = pixels;
     endfunction
 
-    function [7:0] segment_F(input Segments segments, logic [3:0] x, logic [1:0] y);
-        logic [7:0] pixels = 8'h00;
+    function [7:0] segment_F(input Segments segments, bit [3:0] x, bit [1:0] y);
+        bit [7:0] pixels = 8'h00;
         if (segments.individual.f && (x <= 3) && ((y == 0) || (y == 1))) begin
             pixels = pattern_bcef[{x[1:0], y[0]}];
         end
